a891d1c19c59364e38dd38223b831be5c4ecc58a
안치숙 <songweddin@naver.com>
2025-06-30 21:59:07 +0900

    Feat: 스케줄링을 가능하기 위한 @EnableScheduling 추가

--- src/main/java/com/example/data_collector/DataCollectorApplication.java
+++ src/main/java/com/example/data_collector/DataCollectorApplication.java
@@ -2,8 +2,10 @@ package com.example.data_collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DataCollectorApplication {
 
 	public static void main(String[] args) {
