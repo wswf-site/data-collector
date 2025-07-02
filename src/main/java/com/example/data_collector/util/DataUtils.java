80d6cf5cd780ee5aee894dbb88f74b9a2f388959
안치숙 <songweddin@naver.com>
2025-07-02 23:39:29 +0900

    Feat: 유튜브 id 이름 바꾸기

--- src/main/java/com/example/data_collector/util/DataUtils.java
+++ src/main/java/com/example/data_collector/util/DataUtils.java
@@ -7,11 +7,10 @@ import java.util.Map;
 @Slf4j
 public class DataUtils {
     public static final Map<String, String> VIDEO_ID_TO_TEAM = Map.of(
-            "vEuBkPXkfQ4", "MOTIV",
-            "OQPYfEi-nyA", "BUMSUP",
-            "EZrvieOjij0", "AGSQUAD",
-            "t7RN-R5DnZM", "OSAKA_OjoGang",
-            "eHaxR1rmSHo", "RHTokyo"
+            "Eq12SwjR25s", "MOTIV",
+            "8ZpL_uXnYGQ", "BUMSUP",
+            "da68UOXfQ_s", "AGSQUAD",
+            "-DYg4IjGLUk", "OSAKA_OjoGang"
     );
     public static final String UNKNOWN_TEAM = "unknown";
 }
